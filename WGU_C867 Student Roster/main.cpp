df032d58dad25d518c7a301b898d35bd30d5f0da
visavis88 <visavis88@gmail.com>
2023-09-21 22:31:46 -0700

    Initial Commit

---
+++ WGU_C867 Student Roster/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WGU_C867 Student Roster
+//
+//  Created by mykhaylo roslyak on 9/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
